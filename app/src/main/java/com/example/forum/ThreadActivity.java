f8958c779a4887c23527b4b0af3ec273918e1ac8
shaminda1017 <abeyseks@uwindsor.ca>
2024-03-19 21:36:10 -0400

    Added ThreadActivity UI

---
+++ app/src/main/java/com/example/forum/ThreadActivity.java
@@ -0,0 +1,14 @@
+package com.example.forum;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThreadActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_threads);
+    }
+}
